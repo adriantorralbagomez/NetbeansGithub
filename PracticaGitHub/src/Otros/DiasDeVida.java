5e8088f1f7ca627eff4163df755a1a88b847c7cb
adriantorralbagomez <63340932+adriantorralbagomez@users.noreply.github.com>
2020-05-11 19:23:49 +0200

    Cambios en los ficheros

---
+++ PracticaGitHub/src/Otros/DiasDeVida.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Otros;
+import java.util.Scanner;
+/**
+ *
+ * @author adria
+ */
+public class DiasDeVida {
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int edad, dias;
+        System.out.println("Introduce tu edad: ");
+        edad = lector.nextInt();
+        dias = edad * 360;
+        System.out.println("No diré a nadie que tienes " + dias + "  dias!");
+    }
+}
