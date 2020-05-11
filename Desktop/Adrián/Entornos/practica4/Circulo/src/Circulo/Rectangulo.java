3d49af96cbab4f4a2026594401540817292dea1d
Adrian <adriantorralba1@gmail.com>
2020-05-11 10:46:49 +0200

    Añadido rectángulo

---
+++ Desktop/Adrián/Entornos/practica4/Circulo/src/Circulo/Rectangulo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Circulo;
+
+/**
+ *
+ * @author adria
+ */
+public class Rectangulo {
+    private int altura;
+    private int base;
+
+    public Rectangulo(int altura, int base) {
+        this.altura = altura;
+        this.base = base;
+    }
+
+}
