27b18150fdf99cfd346a0af3106aa604f9e3a58d
adriantorralbagomez <63340932+adriantorralbagomez@users.noreply.github.com>
2020-05-11 19:22:45 +0200

    Add files via upload

---
+++ Unidad02-Ejercicios2/src/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Adrián Torralba Gómez
+ * @version 11/05/2020
+ */
+public class HolaMundo {
+    public static void main(String[]args){
+        System.out.println("Hola, mundo!");
+        System.out.println("Hola mundo, de nuevo!");
+    }
+}
